f9bdc2057f3907a3db9d82fdaf1e891c585b2692
Tomas Chalupnik <tchalupnik@tchalupnik.cz>
2020-07-14 09:23:55 +0200

    updated duty config

--- DutyManager/src/Config.lua
+++ DutyManager/src/Config.lua
@@ -1,6 +1,7 @@
 DMConfig = LibStub("AceAddon-3.0"):NewAddon("DMConfig")
 
 DMConfig = {
+    version="0.0.1",
     raidTarget = {
         SKULL="interface/targetingframe/ui-raidtargetingicon_8.blp",
         CROSS="interface/targetingframe/ui-raidtargetingicon_7.blp",
@@ -15,211 +16,211 @@ DMConfig = {
         WARRIOR={
             {
                 path="interface/icons/ability_warrior_battleshout.blp", -- battle shout
-                name="",
+                name="Battle Shout",
             },
             {
                 path="interface/icons/ability_warrior_sunder.blp", -- sunder armor
-                name="",
+                name="Sunder Armor",
             },
             {
                 path="interface/icons/ability_thunderclap.blp", -- thunder clap
-                name="",
+                name="Thunder Clap",
             },
             {
                 path="interface/icons/ability_warrior_warcry.blp", -- demoralizing shout
-                name="",
+                name="Demoralizing Shout",
             },
 
             {
                 path="interface/icons/ability_golemthunderclap.blp", -- intimidating shout
-                name="",
+                name="Intimidating Shout",
             },
 
             {
                 path="interface/icons/inv_gauntlets_04.blp", -- pummel
-                name="",
+                name="Pummel",
             },
         },
         ROGUE={
             {
                 path="interface/icons/inv_misc_herb_16.blp", -- wound poison
-                name="",
+                name="Wound Poison",
             },
             {
                 path="interface/icons/spell_nature_nullifydisease.blp", -- mind numbing poison
-                name="",
+                name="Mind Numbing Poison",
             },
 
             {
                 path="interface/icons/ability_sap.blp", -- sap
-                name="",
+                name="Sap",
             },
             {
                 path="interface/icons/ability_kick.blp", -- kick
-                name="",
+                name="Kick",
             },
             {
                 path="interface/icons/spell_shadow_mindsteal.blp", -- blind
-                name="",
+                name="Blind",
             },
             {
                 path="interface/icons/ability_gouge.blp", -- gouge
-                name="",
+                name="Gouge",
             },
         },
         MAGE={
             {
                 path="interface/icons/spell_holy_arcaneintellect.blp", -- int
-                name="",
+                name="Intellect",
             },
             {
                 path="interface/icons/spell_nature_abolishmagic.blp", -- dampen
-                name="",
+                name="Dampen Magic",
             },
             {
                 path="interface/icons/spell_holy_flashheal.blp", -- amplify
-                name="",
+                name="Amplify Magic",
             },
 
             {
                 path="interface/icons/spell_fire_soulburn.blp", -- improved scorch
-                name="",
+                name="Scorch",
             },
             {
                 path="interface/icons/spell_frost_chillingblast.blp", -- winters chill
-                name="",
+                name="Winter's Chill",
             },
 
             {
                 path="interface/icons/spell_nature_polymorph.blp", -- poly
-                name="",
+                name="Polymorph",
             },
             {
                 path="interface/icons/spell_frost_frostnova.blp", -- frost nova
-                name="",
+                name="Frost Nova",
             },
             {
                 path="interface/icons/spell_frost_iceshock.blp", -- counterspell
-                name="",
+                name="Counterspell",
             },
 
             {
                 path="interface/icons/spell_nature_removecurse.blp", -- remove curse
-                name="",
+                name="Remove Curse",
             },
         },
         PRIEST={
             {
                 path="interface/icons/spell_holy_wordfortitude.blp", -- stam
-                name="",
+                name="Stamina",
             },
             {
                 path="interface/icons/spell_shadow_antishadow.blp", -- shadow protection
-                name="",
+                name="Shadow Protection",
             },
             {
                 path="interface/icons/spell_holy_divinespirit.blp", -- spirit
-                name="",
+                name="Spirit",
             },
             {
                 path="interface/icons/spell_holy_excorcism.blp", -- fear ward
-                name="",
+                name="Fear Ward",
             },
 
             {
                 path="interface/icons/spell_shadow_blackplague.blp", -- shadow weaving
-                name="",
+                name="Shadow Weaving",
             },
 
             {
                 path="interface/icons/spell_nature_slow.blp", -- shackle undead
-                name="",
+                name="Shackle Undead",
             },
             {
                 path="interface/icons/spell_shadow_shadowworddominate.blp", -- mind control
-                name="",
+                name="Mind Control",
             },
             {
                 path="interface/icons/spell_shadow_psychicscream.blp", -- psychic scream
-                name="",
+                name="Psychic Scream",
             },
             {
                 path="interface/icons/spell_shadow_impphaseshift.blp", -- silence
-                name="",
+                name="Silence",
             },
 
             {
                 path="interface/icons/spell_nature_nullifydisease.blp", -- cure disease
-                name="",
+                name="Cure Disease",
             },
             {
                 path="interface/icons/spell_holy_dispelmagic.blp", -- dispel magic
-                name="",
+                name="Dispel Magic",
             },
         },
         WARLOCK={
             {
                 path="interface/icons/spell_shadow_bloodboil.blp", -- blood pact
-                name="",
+                name="Blood Pact",
             },
             {
                 path="interface/icons/inv_misc_orb_04.blp", -- SS
-                name="",
+                name="Soulstone",
             },
             {
                 path="interface/icons/inv_stone_04.blp", -- HS
-                name="",
+                name="Healthstone",
             },
 
             {
                 path="interface/icons/spell_shadow_unholystrength.blp", -- CoR
-                name="",
+                name="Curse of Recklesness",
             },
             {
                 path="interface/icons/spell_shadow_curseofachimonde.blp", -- CoS
-                name="",
+                name="Curse of Shadow",
             },
             {
                 path="interface/icons/spell_shadow_chilltouch.blp", -- CoE
-                name="",
+                name="Curse of Elements",
             },
             {
                 path="interface/icons/spell_shadow_curseofmannoroth.blp", -- CoW
-                name="",
+                name="Curse of Weakness",
             },
             {
                 path="interface/icons/spell_shadow_curseoftounges.blp", -- CoT
-                name="",
+                name="Curse of Tongues",
             },
             {
                 path="interface/icons/spell_shadow_curseofsargeras.blp", -- CoA
-                name="",
+                name="Curse of Agony",
             },
             {
                 path="interface/icons/spell_shadow_auraofdarkness.blp", -- CoD
-                name="",
+                name="Curse of Doom",
             },
 
             {
                 path="interface/icons/spell_shadow_possession.blp", -- fear
-                name="",
+                name="Fear",
             },
             {
                 path="interface/icons/spell_shadow_cripple.blp", -- banish
-                name="",
+                name="Banish",
             },
             {
                 path="interface/icons/spell_shadow_mindrot.blp", -- spell lock
-                name="",
+                name="Spell Lock",
             },
             {
                 path="interface/icons/spell_shadow_mindsteal.blp", -- seduction
-                name="",
+                name="Seduction",
             },
 
             {
                 path="interface/icons/spell_nature_purge.blp", -- devour magic
-                name="",
+                name="Devour Magic",
             },
         },
         HUNTER={
@@ -265,166 +266,166 @@ DMConfig = {
         SHAMAN={
             {
                 path="interface/icons/spell_nature_tremortotem.blp", -- tremor totem
-                name="",
+                name="Tremor Totem",
             },
             {
                 path="interface/icons/spell_nature_strengthofearthtotem02.blp", -- earthbind
-                name="",
+                name="Earthbind Totem",
             },
             {
                 path="interface/icons/spell_frostresistancetotem_01.blp", -- frost resistance
-                name="",
+                name="Frost Resistance Totem",
             },
             {
                 path="interface/icons/spell_fireresistancetotem_01.blp", -- fire resistance
-                name="",
+                name="Fire Resistance Totem",
             },
             {
                 path="interface/icons/spell_frost_summonwaterelemental.blp", -- mana tide
-                name="",
+                name="Mana Tide Totem",
             },
             {
                 path="interface/icons/spell_nature_diseasecleansingtotem.blp", -- disease cleansing
-                name="",
+                name="Disease Cleansing Totem",
             },
             {
                 path="interface/icons/spell_nature_poisoncleansingtotem.blp", -- posion cleansing
-                name="",
+                name="Poison Cleansing Totem",
             },
             {
                 path="interface/icons/spell_nature_natureresistancetotem.blp", -- nature resistance
-                name="",
+                name="Nature Resistance Totem",
             },
             {
                 path="interface/icons/spell_nature_windfury.blp", -- windfury
-                name="",
+                name="Windfury Totem",
             },
             {
                 path="interface/icons/spell_nature_brilliance.blp", -- tranquil air totem
-                name="",
+                name="Tranquil Air Totem",
             },
             {
                 path="interface/icons/spell_nature_invisibilitytotem.blp", -- grace of air
-                name="",
+                name="Grace of Air Totem",
             },
 
             {
                 path="interface/icons/spell_nature_purge.blp", -- purge
-                name="",
+                name="Purge",
             },
         },
         DRUID={
             {
                 path="interface/icons/spell_nature_regeneration.blp", -- mark
-                name="",
+                name="Mark of the Wild",
             },
             {
                 path="interface/icons/spell_nature_thorns.blp", -- thorns
-                name="",
+                name="Thorns",
             },
             {
                 path="interface/icons/spell_nature_reincarnation.blp", -- reinc
-                name="",
+                name="Reincarnation",
             },
 
             {
                 path="interface/icons/spell_nature_faeriefire.blp", -- faeriefire
-                name="",
+                name="Faerie Fire",
             },
             {
                 path="interface/icons/spell_nature_insectswarm.blp", -- insect swarm
-                name="",
+                name="Insect Swarm",
             },
 
             {
                 path="interface/icons/spell_nature_sleep.blp", -- hibernate
-                name="",
+                name="Hibernate",
             },
             {
                 path="interface/icons/spell_nature_stranglevines.blp", -- entangling roots
-                name="",
+                name="Entangling Roots",
             },
 
             {
                 path="interface/icons/spell_nature_nullifypoison_02.blp", -- dispell poison
-                name="",
+                name="Dispel Posion",
             },
             {
                 path="interface/icons/spell_holy_removecurse.blp", -- remove curse
-                name="",
+                name="Remove Curse",
             },
         },
         PALADIN={
             {
                 path="interface/icons/spell_holy_greaterblessingofkings.blp", -- BoM
-                name="",
+                name="Blessing of Might",
             },
             {
                 path="interface/icons/spell_holy_greaterblessingofsalvation.blp", -- BoS
-                name="",
+                name="Blessing of Salvation",
             },
             {
                 path="interface/icons/spell_holy_greaterblessingofwisdom.blp", -- wisdom
-                name="",
+                name="Blessing of Wisdom",
             },
             {
                 path="interface/icons/spell_magic_greaterblessingofkings.blp", -- BoK
-                name="",
+                name="Blessing of Kings",
             },
             {
                 path="interface/icons/spell_holy_greaterblessingoflight.blp", -- BoL
-                name="",
+                name="Blessing of Light",
             },
             {
                 path="interface/icons/spell_holy_greaterblessingofsanctuary.blp", -- sanctuary
-                name="",
+                name="Blessing of Sanctuary",
             },
             {
                 path="interface/icons/spell_holy_devotionaura.blp", -- devotion aura
-                name="",
+                name="Devotion Aura",
             },
             {
                 path="interface/icons/spell_holy_mindsooth.blp", -- concentration aura
-                name="",
+                name="Concentration Aura",
             },
             {
                 path="interface/icons/spell_holy_auraoflight.blp", -- retribution aura
-                name="",
+                name="Retribution Aura",
             },
             {
                 path="interface/icons/spell_fire_sealoffire.blp", -- FR aura
-                name="",
+                name="Fire Resistance Aura",
             },
             {
                 path="interface/icons/spell_frost_wizardmark.blp", -- frost res aura
-                name="",
+                name="Frost Resistance Aura",
             },
             {
                 path="interface/icons/spell_shadow_sealofkings.blp", -- SR aura
-                name="",
+                name="Shadow Resist Aura",
             },
 
             {
                 path="interface/icons/spell_holy_healingaura.blp", -- judgement of light
-                name="",
+                name="Judgement of Light",
             },
             {
                 path="interface/icons/spell_holy_righteousnessaura.blp", -- judgement of wisdom
-                name="",
+                name="Judgement of Wisdom",
             },
 
             {
                 path="interface/icons/spell_holy_sealofmight.blp", -- hammer of justice
-                name="",
+                name="Hammer of Justice",
             },
             {
                 path="interface/icons/spell_holy_prayerofhealing.blp", -- repentance
-                name="",
+                name="Repentance",
             },
 
             {
                 path="interface/icons/spell_holy_renew.blp", -- cleanse
-                name="",
+                name="Cleanse",
             },
         },
     },
