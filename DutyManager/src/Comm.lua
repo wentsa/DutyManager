caea9e5bb6210831410771ace0c30f700a371268
Tomas Chalupnik <tchalupnik@tchalupnik.cz>
2020-07-14 09:30:54 +0200

    comm check and broadcast

--- DutyManager/src/Comm.lua
+++ DutyManager/src/Comm.lua
@@ -4,17 +4,23 @@ local LibAceSerializer = LibStub:GetLibrary("AceSerializer-3.0")
 
 local TYPES = {
     SET_DUTY="DM_Duty",
-    SET_CONFIRMATION="DM_Confirmation"
+    SET_CONFIRMATION="DM_Confirmation",
+    CHECK_ADDON="DM_Check",
+    CHECK_BROADCAST="DM_Broadcast",
 }
 
 DMComm.callbacks = {
     onDutySet=nil,
     onConfirmationSet=nil,
+    onCheck=nil,
+    onCheckBroadcast=nil,
 }
 
 function DMComm:OnEnable()
     DMComm:RegisterComm(TYPES.SET_DUTY, "OnDutySet")
     DMComm:RegisterComm(TYPES.SET_CONFIRMATION, "OnConfirmationSet")
+    DMComm:RegisterComm(TYPES.CHECK_ADDON, "OnCheck")
+    DMComm:RegisterComm(TYPES.CHECK_BROADCAST, "OnCheckBroadcast")
 end
 
 function DMComm:setCallback(name, cb)
@@ -40,6 +46,20 @@ function DMComm:OnConfirmationSet(prefix, message, distribution, sender)
     end
 end
 
+function DMComm:OnCheck(prefix, message, distribution, sender)
+    if (DMComm.callbacks.onCheck ~= nil) then
+        DMComm.callbacks.onCheck(sender)
+    end
+end
+
+function DMComm:OnCheckBroadcast(prefix, message, distribution, sender)
+    local success, data = LibAceSerializer:Deserialize(message)
+    if (success and DMComm.callbacks.onCheckBroadcast ~= nil) then
+        DMComm.callbacks.onCheckBroadcast(sender, data)
+    end
+end
+
+
 -- duty={id, manager, assignee, task, target, note, icon, taskIcon}
 function DMComm:SetDuty(duty, onError)
     if (DMUtils:isInYourGroup(duty.assignee)) then
@@ -57,4 +77,13 @@ end
 function DMComm:SetConfirmation(duty, confirmed)
     local data = LibAceSerializer:Serialize({ id=duty.id, confirmed=confirmed });
     DMComm:SendCommMessage(TYPES.SET_CONFIRMATION, data, "WHISPER", duty.manager)
+end
+
+function DMComm:Check(who)
+    DMComm:SendCommMessage(TYPES.CHECK_ADDON, "", "WHISPER", who)
+end
+
+function DMComm:CheckBroadcast(to, obj)
+    local data = LibAceSerializer:Serialize(obj);
+    DMComm:SendCommMessage(TYPES.CHECK_BROADCAST, data, "RAID")
 end
